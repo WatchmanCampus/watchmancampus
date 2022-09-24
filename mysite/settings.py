3c7ba57e5b5b6dcf8d17184b5cd2a601438f3f4b
CaptainVee <captainvee3@gmail.com>
2022-09-24 12:46:05 +0100

    dfd

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
