bfb2c7039f09afd7adcc83d1423eb7496491ae55
CaptainVee <captainvee3@gmail.com>
2022-10-01 17:14:59 +0100

    test againn

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,8 +130,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
