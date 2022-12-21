7f9c4d5fed09f6d230f18123893b78e431eb2015
CaptainVee <captainvee3@gmail.com>
2022-12-21 19:00:36 +0100

    feat: new landing page

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 
 def home_page(request):
-    return render(request, "pages/home_page.html", {})
+    return render(request, "pages/index.html", {})
